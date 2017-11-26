cfe5423f53a7aaa8a3e726fb0b6a21e483c7512b
Shadow Grimes <grimeswongatwork@gmail.com>
2017-11-26 18:07:05 +0000

    profile form basically acheived

--- hew5/resources/views/main.blade.php
+++ hew5/resources/views/main.blade.php
@@ -7,17 +7,62 @@
 @section('content')
 
             <div class="header">
-                <h4 class="col-md-2 offset-md-2">University Of Queensland</h4>
+                <p class="col-sm-4 offset-sm-2">University Of Queensland</p>
             </div>
             <div>
                 <img id="image" src="img/uq.jpg" />
-                <div class="col-md-8 offset-md-2" id="mid-block">
+                <div class="col-sm-8 offset-sm-2" id="mid-block">
                     <p id="mid-title">Welcome to UQ Researcher Site<p>
                 </div>
             </div>
-            <div id="content">
-                <p id="content-title">Content title</p>
-                <p>Some text here</p>
+            <div class="main-content">
+                <p class="main-content-title">Researcher Profile</p>
+                <p class="main-content-subtitle">Please enter your profile details<p>
+
+                  <form class="col-sm-8" id="profile-form" method="GET" action="">  <!-- need to change POST later -->
+
+                    <div class="row">
+                      <div class="form-group col-sm-6">
+                        <label class="control-label">First Name</label>
+                        <input class="form-control" placeholder="First Name">
+                      </div>
+                      <div class="form-group col-sm-6">
+                        <label class="control-label">Last Name</label>
+                        <input class="form-control" placeholder="Last Name">
+                      </div>
+                    </div>
+
+                    <div class="row">
+                      <div class="form-group col-sm-6">
+                        <label class="control-label">Position</label>
+                        <input class="form-control" placeholder="Position">
+                      </div>
+                      <div class="form-group col-sm-6">
+                        <label class="control-label">Email</label>
+                        <input class="form-control" placeholder="name@example.com">
+                      </div>
+                    </div>
+
+                    <div class="row">
+                      <div class="form-group col-sm-12">
+                        <label class="control-label">Biography</label>
+                        <textarea class="form-control" rows="5" placeholder="something about yourself"></textarea>
+                      </div>
+                    </div>
+
+                    <div class="row">
+                      <div class="form-group col-sm-12">
+                        <label class="control-label">Profile Image</label>
+                        <input type="file" class="form-control-file">
+                    </div>
+
+                    <button class="btn btn-success btn-lg offset-md-3 col-sm-12 col-md-6">Submit</button>
+
+
+                  </form>
+                </div>
             </div>
+
+
 
 @endsection
